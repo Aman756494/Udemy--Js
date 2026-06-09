fd1ffcf22680eeaeff75e5586d0047777bf0f976
Aman756494 <kraman9262@gmail.com>
2026-06-09 15:55:25 +0530

    .

---
+++ part9_adv/appModule.js
@@ -0,0 +1,11 @@
+//Module import file
+
+//default import
+import multiply from "./72-mathOperationsM.js";
+
+//named import
+import { add,subtract } from "./72-mathOperationsM.js";
+
+console.log(multiply(2, 4));
+console.log(add(3, 5));
+
